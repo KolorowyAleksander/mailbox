6eb3d7d974515a3167a060b5708fbbe200d562c9
KolorowyAleksander <KolorowyAleksander@users.noreply.github.com>
2017-01-04 17:53:10 +0100

    changed project structure to allow multiple targets

---
+++ broker/src/main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main(int argc, char * argv[]) {
+  std::cout << "I'm a server!" << std::endl;
+  return 0;
+}
---
+++ client/src/main.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+  printf("I'm a client", number);
+  return 0;
+}
